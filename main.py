a1a1ef65f9a4b9f118d86b39dc92b8e4b88d9aaf
cookingnoob <110266985+cookingnoob@users.noreply.github.com>
2024-12-29 19:21:08 -0600

    add screen feature

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen.fill((0, 0, 0))
+    pygame.display.flip()
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
